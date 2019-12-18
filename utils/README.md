d1dcf1ad10d83538170845e30d8cd3861923ee33
zxfjd3g <258147149@qq.com>
2019-12-18 14:12:03 +0800

    utils

---
+++ utils/README.md
@@ -0,0 +1,18 @@
+# 工具函数
+## 1. 函数的call() / apply() / bind()
+## 2. 数组声明式系列方法: reduce()/map()/filter()/find()/findIndex()/every()/some()
+## 3. 函数的节流(throttle)与防抖(debounce)
+## 4. 数组去重(unique)
+## 5. 数组合并(concat)与切片(slice)
+## 6. 数组扁平化(flatten)
+## 7. 数组取真与分块
+## 8. 数组取差异与合并
+## 9. 自定义new和instanceof
+## 10. 删除数组中部分元素
+## 11. 得到数组的部分元素
+
+# 工具库
+## 1. 消息订阅与发布库: pub-sub
+## 2. 事件总线: event-bus
+
+
---
+++ utils/event-bus.js
@@ -0,0 +1,51 @@
+/* 
+自定义事件总线的模块
+*/
+(function (window) {
+  const eventBus = {}
+
+  /* 
+  {
+    add:  [callback1, callback2]
+    delete: [callback3]
+  }
+  */
+  let callbacksObj = {}  
+
+  /* 
+  绑定事件监听
+  */
+  eventBus.on = function (eventName, callback) {
+    const callbacks = callbacksObj[eventName]
+    if (callbacks) {
+      callbacks.push(callback)
+    } else {
+      callbacksObj[eventName] = [callback]
+    }
+  }
+  
+  /* 
+  分发事件
+  */
+  eventBus.emit = function (eventName, data) {
+    const callbacks = callbacksObj[eventName]
+    if (callbacks && callbacks.length>0) {
+      callbacks.forEach(callback => {
+        callback(data)
+      })
+    }
+  }
+
+  /* 
+  移除事件监听
+  */
+  eventBus.off = function (eventName) {
+    if (eventName) {
+      delete callbacksObj[eventName]
+    } else {
+      callbacksObj = {}
+    }
+  }
+
+  window.eventBus = eventBus
+})(window)
