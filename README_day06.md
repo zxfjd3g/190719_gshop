aea4eac94ec88ff1056c9539ddeed2fd47f60b46
zxfjd3g <258147149@qq.com>
2019-12-20 15:38:51 +0800

    ratings与info

--- README_day06.md
+++ README_day06.md
@@ -7,8 +7,8 @@
   c. 处理带params参数的子路由和<router-link>
   d: 自动跳转路由: redirect配置
   e. 在调用push()进行路由跳转时(需要携带参数)   ---面试可重点说
-    直接路径path: 自动跳转有效
-    配置对象指定name/params: 自动跳转失效
+      直接路径path: 自动跳转有效
+      配置对象指定name/params: 自动跳转失效
 
 ### 2). 根据指定的商家id获取对应的商家数据
   a. 完成mock接口
--- src/main.js
+++ src/main.js
@@ -8,12 +8,14 @@ import router from './router'
 import Header from './components/Header/Header.vue'
 import Star from './components/Star/Star.vue'
 import CartControl from './components/CartControl/CartControl.vue'
+import Split from './components/Split/Split.vue'
 import store from './vuex/store'
 import './validate'
 import * as API from '@/api'
 import i18n from './i18n'
 import './mock/mock-server'
 import loading from '@/common/images/loading.gif'
+import './filters'
 
 Vue.use(VueLazyload, {
   loading,  // 在要显示的图片没有加载到前显示
@@ -27,6 +29,7 @@ Vue.prototype.$API = API
 Vue.component('Header', Header)
 Vue.component('Star', Star)
 Vue.component('CartControl', CartControl)
+Vue.component('Split', Split)
 Vue.component(Button.name, Button)  // mt-button
 
 new Vue({
